3ed36f9f42d51886598cc8bf83a37ff729130d54
Gun <you@example.com>
2024-08-08 18:29:27 +0800

    feat: 刷新菜单自动选中

--- src/layout/sider/index.jsx
+++ src/layout/sider/index.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Menu } from "antd";
 
 import { LaptopOutlined, UserOutlined } from "@ant-design/icons";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 const keys = ["/home", "/option"];
 const items2 = [UserOutlined, LaptopOutlined].map((icon, index) => {
   const key = String(index + 1);
@@ -10,31 +10,31 @@ const items2 = [UserOutlined, LaptopOutlined].map((icon, index) => {
     key: keys[index % 2],
     icon: React.createElement(icon),
     label: `subnav ${key}`,
-    // children: new Array(4).fill(null).map((_, j) => {
-    //   const subKey = index * 4 + j + 1;
-    //   return {
-    //     key: subKey,
-    //     label: `option${subKey}`,
-    //   };
-    // }),
   };
 });
+
 function asider() {
   const navigate = useNavigate();
-  const handleSelect = (e) => {
-    navigate(e.key);
+  const location = useLocation();
+  useEffect(() => {
+    // 当路由改变时，更新选中的菜单项
+    setSelectedKeys([location.pathname]);
+  }, [location.pathname]);
+  const [selectedKeys, setSelectedKeys] = useState([]);
+
+  const handleSelect = ({ keyPath }) => {
+    navigate(keyPath[0]);
   };
   return (
     <Menu
       mode="inline"
-      defaultSelectedKeys={["2"]}
-      defaultOpenKeys={["sub1"]}
       style={{
         height: "100%",
         borderRight: 0,
       }}
+      selectedKeys={selectedKeys}
       items={items2}
-      onSelect={handleSelect}
+      onClick={handleSelect}
     />
   );
 }
